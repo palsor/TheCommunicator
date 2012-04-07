61f8ce23d712311a892a924386c2816f32a837e3
Monsterkruck <p.m.a.c.k.3.y@gmail.com>
2012-04-07 12:38:31 -0500

    Struct sync w/Driver

--- Structs.h
+++ Structs.h
@@ -28,7 +28,8 @@ struct SensorData {
   float yaw_e;          // Y degrees in earth's frame of reference
   float roll_e;         // R degrees in earth's frame of reference
   float pressAltitude;  // Palt meters
-  float airspeed_e[3];  // AS m/s in earth's frame of reference
+  float airspeedRaw;    // AS magnitude
+  float airspeed_e[3];  // AS3 m/s in earth's frame of reference
   float gyro_b[3];      // rotation about each axis in body's frame of reference
   float battVoltage;    // V volts
   bool gpsUpdated;      // Gu 1 = GPS data was updated, 0 = GPS was not updated
